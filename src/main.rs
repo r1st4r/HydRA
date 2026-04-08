a26d4bb0c076db1e6dce30eda876db989f06e99f
r1st4r <2291683392@qq.com>
2026-04-08 21:07:21 +0800

    define some variable

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,19 @@ fn main() {
     for i in 0.2_i32.pow(10) {
         b_tree.push(BlsFr::rand(rng));
     }
+    let mut root = vec![];
+
+    let hasher = PoseidonSetup(Curve::Bls12_381, 5, 3);
+    let pk = BlsFr::rand(rng);
+    let sk = BlsFr::rand(rng);
+    let time = BlsFr::rand(rng);
+    let period = BlsFr::rand(rng);
+    let ar = BlsFr::rand(rng);
+
+    let c = hasher.hash(&[ar, sk]).unwrap();
+	let leaf = hasher.hash(&[c, pk]).unwrap();
+	let output_1 = hasher.hash(&[pk, ar]).unwrap();
+	let output_2 = hasher.hash(&[output_1,sk]).unwrap();
+	let output_3 = hasher.hash(&[output_2,time]).unwrap();
+	let output = hasher.hash(&[output_3,period]).unwrap();
 }
