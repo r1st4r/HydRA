32c36e55cee6ed76354997fe1abdcc66e0c5a1c3
r1st4r <2291683392@qq.com>
2026-04-13 21:19:41 +0800

    update shrubtree

--- src/shurbtree/mod.rs
+++ src/shurbtree/mod.rs
@@ -4,3 +4,24 @@ use arkworks_native_gadgets::poseidon::{
     };
 
 use rayon::{prelude::*, range};
+
+pub fn BuildShrubs(mut root: &mut Vec<BlsFr>, leaf: &[BlsFr], mut path: &mut Vec<BlsFr>, hasher: Poseidon::<BlsFr>) {
+    let mut temp = vec![];
+
+    for i in (0..leaf.len()).step_by(2) {
+        if i == leaf.len()-1 || i == leaf.len()-2 {
+            root.push(leaf[i].clone());
+        }
+
+        if i < leaf.len()-1 {
+            temp.push(hasher.hash(&[leaf[i],leaf[i+1]]).unwrap());
+        }
+        if leaf.len() >= 2 && i == leaf.len() - 2 {
+            path.push(leaf[i].clone());
+        }
+    }
+
+    if !temp.is_empty() {
+        BuildShrubs( &mut root, &temp, &mut path, hasher);
+    }
+}
