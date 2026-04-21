d0e40cd2bd2d5c98a0403e45ef8526520fb57d15
r1st4r <2291683392@qq.com>
2026-04-21 20:26:41 +0800

    update zkcircuit

--- src/zkcircuit/mod.rs
+++ src/zkcircuit/mod.rs
@@ -2,3 +2,18 @@ use ark_ff::PrimeField;
 use ark_r1cs_std::{alloc::AllocVar, eq::EqGadget, fields::fp::FpVar};
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
 use arkworks_r1cs_gadgets::poseidon::FieldHasherGadget;
+
+#[derive(Copy, Debug)]
+pub struct TestCircuit<'a, F: PrimeField, HG: FieldHasherGadget<F>> {
+    pk: F,
+    sk: F,
+    ar: F,
+    time: F,
+    period: F,
+    output: F,
+    root: F,
+    path: F,
+    path: &'a [F],
+    tag: &'a [bool],
+    hasher: HG::Native,
+}
