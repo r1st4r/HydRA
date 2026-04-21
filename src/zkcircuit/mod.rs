d4d075a5c0281c5d30a70e12a7a41adccefe85cc
r1st4r <2291683392@qq.com>
2026-04-21 20:07:57 +0800

    add zkcircuit

---
+++ src/zkcircuit/mod.rs
@@ -0,0 +1,4 @@
+use ark_ff::PrimeField;
+use ark_r1cs_std::{alloc::AllocVar, eq::EqGadget, fields::fp::FpVar};
+use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
+use arkworks_r1cs_gadgets::poseidon::FieldHasherGadget;
