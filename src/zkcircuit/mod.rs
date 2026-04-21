0e9811cda7bc7e009e14ff9434d1d11f4afcf9aa
r1st4r <2291683392@qq.com>
2026-04-21 20:48:38 +0800

    add func

--- src/zkcircuit/mod.rs
+++ src/zkcircuit/mod.rs
@@ -17,3 +17,10 @@ pub struct TestCircuit<'a, F: PrimeField, HG: FieldHasherGadget<F>> {
     tag: &'a [bool],
     hasher: HG::Native,
 }
+
+#[allow(dead_code)]
+impl<'a, F: PrimeField, HG: FieldHasherGadget<F>> TestCircuit<'a, F, HG> {
+    pub fn new(pk: F, sk: F, ar: F, time: F, period: F, output: F, root: F, path: &'a [F], tag: &'a [bool], hasher: HG::Native) -> Self {
+        Self {pk, sk, ar, time, period, output, root, path, tag, hasher}
+    }
+}
